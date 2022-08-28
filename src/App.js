9402595a44dd95d520695416fa5b93055566b454
SahatMarisi <sahat346@gmail.com>
2022-08-28 14:33:48 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css' 
+import Header from './components/Header'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+
+  );
+}
+
+
+
+
+export default App;
